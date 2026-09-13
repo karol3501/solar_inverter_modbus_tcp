d763ce95411186c9e031f9347768b26c9c4ddbb6
karol3501 <73785636+karol3501@users.noreply.github.com>
2026-09-13 21:48:09 +0300

    Remove obsolete debug attribute patching

--- custom_components/solar_inverter_modbus_tcp/__init__.py
+++ custom_components/solar_inverter_modbus_tcp/__init__.py
@@ -28,9 +28,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await coordinator.async_config_entry_first_refresh()
     entry.runtime_data = coordinator
 
-    from . import debug_attributes, sensor
-    debug_attributes.patch_sensor_entities(sensor)
-
     entry.async_on_unload(entry.add_update_listener(_async_options_updated))
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
